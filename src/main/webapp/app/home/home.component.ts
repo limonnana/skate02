9fea3c2b809a863b001ea5bba87228eff91b81da
limonnana <rosenzvaig@gmail.com>
2021-03-23 19:19:08 +0200

    refactoring and event

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
-
+import { HttpResponse } from '@angular/common/http';
 import { LoginModalService } from 'app/core/login/login-modal.service';
 import { AccountService } from 'app/core/auth/account.service';
 import { Account } from 'app/core/user/account.model';
+import { HomeService } from './home.service';
+import { IEvent } from 'app/shared/model/event.model';
 
 @Component({
   selector: 'jhi-home',
@@ -13,10 +15,12 @@ import { Account } from 'app/core/user/account.model';
 export class HomeComponent implements OnInit, OnDestroy {
   account: Account | null = null;
   authSubscription?: Subscription;
+  event?: IEvent | null;
 
-  constructor(private accountService: AccountService, private loginModalService: LoginModalService) {}
+  constructor(private accountService: AccountService, private homeService: HomeService, private loginModalService: LoginModalService) {}
 
   ngOnInit(): void {
+    this.getActiveEvent();
     this.authSubscription = this.accountService.getAuthenticationState().subscribe(account => (this.account = account));
   }
 
@@ -26,6 +30,10 @@ export class HomeComponent implements OnInit, OnDestroy {
 
   login(): void {
     this.loginModalService.open();
+  }
+
+  getActiveEvent(): void {
+    this.homeService.getActiveEvent().subscribe((res: HttpResponse<IEvent>) => (this.event = res.body));
   }
 
   ngOnDestroy(): void {
